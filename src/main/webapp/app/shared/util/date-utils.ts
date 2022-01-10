358af6e50476feedbd7ba008daaa141d133d946b
adamskipiotr <adamski-piotrek@wp.pl>
2022-01-10 19:41:39 +0100

    Popraw formatowanie Godzin i minut

--- src/main/webapp/app/shared/util/date-utils.ts
+++ src/main/webapp/app/shared/util/date-utils.ts
@@ -8,6 +8,6 @@ export const convertDateTimeToServer = date => (date ? moment(date, APP_LOCAL_DA
 
 export const extractDate = date => (date ? moment(date).format('YYYY-MM-DD') : null);
 
-export const extractTime = date => (date ? moment(date).format('HH:MM') : null);
+export const extractTime = date => (date ? moment(date).utc().format('HH:mm') : null);
 
 export const displayDefaultDateTime = () => moment().startOf('day').format(APP_LOCAL_DATETIME_FORMAT);
